7421c4612713d531582ea1992bc8bdd415786661
kate <2951525@gmail.com>
2022-02-09 00:54:18 +0300

    Commit 2: задания 3 и 4 выполнены

--- src/main/java/HW2/HomeWork2.java
+++ src/main/java/HW2/HomeWork2.java
@@ -2,13 +2,19 @@ package HW2;
 
 public class HomeWork2 {
     public static void main(String[] args) {
+        int x = -7;
+        int y = 0;
+        tenToTwenty(x,y);
+        isItPositiveOrNegative(x);
+        isItReallyNegative(x);
+        String Greet = "Привет, мир!";
+        int z = 5;
+        printXTimes(Greet, z);
 
     }
     //region Task1
     public static boolean tenToTwenty(int a, int b){
-        if (10 <= (a + b) && (a+b) >= 20) {
-           return true;
-        } else return false;
+        return 10 <= (a + b) && (a + b) >= 20;
     }
     //endregion
     //region Task2
@@ -17,6 +23,20 @@ public class HomeWork2 {
             System.out.println("Число положительное");
         } else {
             System.out.println("Число отрицательное");
+        }
+    }
+    //endregion
+    //region Task3
+    public static boolean isItReallyNegative(int a){
+        if (a<0) {
+            return true;
+        } else return false;
+    }
+    //endregion
+    //region Task4
+    public static void printXTimes(String text, int a) {
+        for (int i = a; i>0; i--){
+            System.out.println(text);
         }
     }
     //endregion
