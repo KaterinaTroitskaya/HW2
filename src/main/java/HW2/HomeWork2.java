032d9da2c12d137ffea716c1effeedb781c9c26f
kate <2951525@gmail.com>
2022-02-09 00:40:07 +0300

    Commit 1: задания 1 и 2 выполнены

--- src/main/java/HW2/HomeWork2.java
+++ src/main/java/HW2/HomeWork2.java
@@ -1,4 +1,23 @@
 package HW2;
 
 public class HomeWork2 {
+    public static void main(String[] args) {
+
+    }
+    //region Task1
+    public static boolean tenToTwenty(int a, int b){
+        if (10 <= (a + b) && (a+b) >= 20) {
+           return true;
+        } else return false;
+    }
+    //endregion
+    //region Task2
+    public static void isItPositiveOrNegative(int a) {
+        if (a >= 0) {
+            System.out.println("Число положительное");
+        } else {
+            System.out.println("Число отрицательное");
+        }
+    }
+    //endregion
 }
